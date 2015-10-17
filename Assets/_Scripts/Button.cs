c674f44c76116e5fef8b74b03cea206648b4b16d
Ian Pudney <ian.pudney.git@gmail.com>
2015-10-17 00:13:33 -0400

    Added button

--- Assets/_Scripts/Button.cs
+++ Assets/_Scripts/Button.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Button : MonoBehaviour {
 	public bool deadMan = false;
+	public GameObject target;
+
+	int pressTrack = 0;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,10 +14,21 @@ public class Button : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(deadMan && pressTrack > 0) {
+			pressTrack--;
+			if(pressTrack == 0) {
+				Debug.Log ("Button Released");
+				target.gameObject.SendMessage("ButtonReleased", this);
+			}
+		}
 	}
 
 	void OnPlayerHit(ControllerColliderHit coll) {
-		Debug.Log (coll.gameObject);
+		if(pressTrack == 0) {
+			pressTrack = 2;
+			Debug.Log ("Button Pressed");
+			target.gameObject.SendMessage("ButtonPressed", this);
+		}
+		pressTrack = 2;
 	}
 }
