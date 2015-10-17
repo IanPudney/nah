5d8ee1a3154d79319639ce821ece2b936c8b2f00
Ian Pudney <ian.pudney.git@gmail.com>
2015-10-16 22:29:23 -0400

    Added code for managing duplication and differences

---
+++ Assets/_Scripts/CameraManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : Difference {
+
+	// Use this for initialization
+	void Start () {
+		if(isPlayer1) {
+			gameObject.GetComponent<Camera>().rect = new Rect(0, 0, 0.5f, 1);
+		} else {
+			gameObject.GetComponent<Camera>().rect = new Rect(0.5f, 0, 0.5f, 1);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/_Scripts/World.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+public class World : MonoBehaviour {
+	bool isPlayer1 = true;
+	// Use this for initialization
+	void Start () {
+		if(isPlayer1) {
+			GameObject duplicate = GameObject.Instantiate(gameObject, 
+			                                              transform.localPosition + new Vector3(1000, 0, 0), 
+			                                              transform.rotation) as GameObject;
+			duplicate.GetComponent<World>().isPlayer1 = false;
+			foreach(Difference diff in duplicate.GetComponentsInChildren<Difference>()) {
+				diff.isPlayer1 = false;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
