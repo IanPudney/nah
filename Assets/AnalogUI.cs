84a0465560452d53ed8b9a9f744cdb564d38b438
caidavid <davidcai@umich.edu>
2015-10-17 22:52:47 -0400

    Analogue UI

---
+++ Assets/AnalogUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnalogUI : Difference {
+
+    Player playerParent;
+
+	// Use this for initialization
+	void Start () {
+        Player[] players = GameObject.FindObjectsOfType<Player>();
+        foreach (Player player in players) {
+            if (player.isPlayer1 == isPlayer1) {
+                playerParent = player;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = playerParent.gameObject.transform.position;
+	}
+}
