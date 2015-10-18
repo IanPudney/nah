c350cf17f543971872a2b2ae43bd933076994f48
Ian Pudney <ian.pudney.git@gmail.com>
2015-10-17 20:38:44 -0400

    Initial commit of otherization code

---
+++ Assets/_Scripts/ExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ExtensionMethods
+{
+	public static T GetOther<T>(this T obj) where T : MonoBehaviour
+	{
+		GameObject other = obj.GetComponent<Otherizer>().other;
+		if(other == null) {
+			throw new UnityException("This object does not have a counterpart");
+		}
+		return other.GetComponent<T>();
+	}
+}
