2228af4a1eef79232e8fdaf06248ede4240981b6
Ian Pudney <ian.pudney.git@gmail.com>
2015-10-18 16:01:28 -0400

    Adding background prefab

---
+++ Assets/_Scripts/BackgroundPopulate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundPopulate : MonoBehaviour {
+	public GameObject BackgroundCube;
+	public float radius;
+	public float radiusDelta;
+	public int count;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < count; ++i) {
+			GameObject backgroundCube = GameObject.Instantiate(BackgroundCube);
+			backgroundCube.transform.parent = transform;
+			Vector3 target = Random.onUnitSphere * (radius + Random.Range(-radiusDelta, radiusDelta));
+			backgroundCube.transform.localPosition = target;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/_Scripts/BackgroundRotate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundRotate : MonoBehaviour {
+	public float period = 5f;
+	public float periodDelta = 1f;
+	float actualPeriod;
+	Vector3 rotationAxis;
+	Color color = new Color();
+	// Use this for initialization
+	void Start () {
+		actualPeriod = Random.Range(-periodDelta, periodDelta) + period;
+		rotationAxis = Random.onUnitSphere;
+		Vector3 tempColor = Random.onUnitSphere;
+		color.r = tempColor.x / 2;
+		color.g = tempColor.y / 2;
+		color.b = tempColor.z / 2;
+		gameObject.GetComponent<Renderer>().material.color = color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localRotation = Quaternion.AngleAxis(360f * Time.deltaTime / actualPeriod, rotationAxis) * transform.localRotation;
+	}
+}
